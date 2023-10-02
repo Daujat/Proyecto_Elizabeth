761a5f6f937558ff76ab2f41a58e61fe87f21e4f
Your Name <you@example.com>
2023-10-02 14:45:01 -0500

    aea

--- lab06/lab06/settings.py
+++ lab06/lab06/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tienda.apps.TiendaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
