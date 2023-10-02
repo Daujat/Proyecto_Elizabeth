7156e49c9c08c778de1618789a808a821fa27686
Your Name <you@example.com>
2023-10-02 16:11:43 -0500

    tmr

--- lab06/tienda/views.py
+++ lab06/tienda/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+
+from .models import Producto
 
 # Create your views here.
+
+def index(request):
+    producto_list = Producto.objects.order_by('nombre')[:6]
+    context = {'product_list': producto_list}
+    return render(request,'index.html', context)
+
+def producto(request, producto_id):
+    producto = get_list_or_404(Producto, pk=producto_id)
+    return render(request,'producto.html', {'producto': producto})
